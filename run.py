051486c968e82e5dd3f27971332df64a137a8c52
bencarnes <bjcarnes@outlook.com>
2019-08-13 22:16:40 -0400

    Corrected typo in original translation. Verse 3l should be verse 31.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import usfm_writer
 
 def main():
 
-	bible_items, errors = parser.extract_model('content_indented_no_noise.xml')
+	bible_items, errors = parser.extract_model('content_indented_no_noise_manual_corrections.xml')
 
 	usfm_writer.write(bible_items, '../usfm_out_rev2')
 
