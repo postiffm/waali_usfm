18eca1516963f519e5c1439fc65bdd3b4a55e01a
Matt Postiff <postiffm@umich.edu>
2019-08-14 22:37:39 -0400

    Add run instructions and verbose output.

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import waali_parser as parser
 import usfm_writer
 
 def main():
+	xml_in = 'content_indented_no_noise_manual_corrections.xml'
+	usfm_out = '../usfm_out_rev2'
 
+	print ("Loading input: " + xml_in)
 	bible_items, errors = parser.extract_model('content_indented_no_noise_manual_corrections.xml')
 
+	print ("Writing output: " + usfm_out)
 	usfm_writer.write(bible_items, '../usfm_out_rev2')
 
 	for e in errors:
